e75804ca63d8ee9d0c256e4eb86f9d6afc0fe9eb
Alexander Lopatin <sbar.geek@gmail.com>
2012-10-21 00:39:42 +0400

    android dir structure update

--- builds/android/build.sh
+++ builds/android/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+set -e
+
+# FIXME
+cd ~/coding/github/ponic/builds/android
+
+cp -r ../../src/* jni/
+rm -fr jni/platforms/linux/
+
 android update project --name ponic --path . --target "android-15"
 ndk-build
 ant debug
+
 webput bin/*-debug.apk b.apk
--- builds/android/jni/Android.mk
+++ builds/android/jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := libponic
 LOCAL_CFLAGS    := -Werror
-LOCAL_SRC_FILES := gl_code.cpp
+LOCAL_SRC_FILES := engine/platforms/android/main.cpp
 LOCAL_LDLIBS    := -llog -lGLESv2
 
 include $(BUILD_SHARED_LIBRARY)
