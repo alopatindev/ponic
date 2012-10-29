1f7357e82c4a8f5a2357ad4c21c0986d1c4ff8d5
Alexander Lopatin <sbar.geek@gmail.com>
2012-10-30 00:50:06 +0400

    turned off exceptions

--- builds/android/jni/Android.mk
+++ builds/android/jni/Android.mk
@@ -5,7 +5,8 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := libponic
-LOCAL_CFLAGS    := -Werror -D_DEBUG -DANDROID -DPUGIXML_NO_EXCEPTIONS \
+LOCAL_CFLAGS    := -fno-exceptions -Werror \
+                   -D_DEBUG -DANDROID -DPUGIXML_NO_EXCEPTIONS \
                    -g -rdynamic \
                    -Ijni/engine/
 LOCAL_SRC_FILES := engine/platforms/android/main.cpp \
